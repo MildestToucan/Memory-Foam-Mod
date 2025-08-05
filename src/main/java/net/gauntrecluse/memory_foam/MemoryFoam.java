6f651a591ecee4835a1c72ffd52fd49a7da1f7b4
MildToucan <introvertpale@gmail.com>
2025-08-05 14:20:36 +0200

    Added playerWakeUpEvent handler

--- src/main/java/net/gauntrecluse/memory_foam/MemoryFoam.java
+++ src/main/java/net/gauntrecluse/memory_foam/MemoryFoam.java
@@ -1,13 +1,10 @@
 package net.gauntrecluse.memory_foam;
 
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.event.entity.player.PlayerWakeUpEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
@@ -16,9 +13,7 @@ import org.slf4j.Logger;
 @Mod(MemoryFoam.MODID)
 public class MemoryFoam {
 
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "memory_foam";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public MemoryFoam() {
@@ -29,22 +24,17 @@ public class MemoryFoam {
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+
+        MinecraftForge.EVENT_BUS.addListener(this::playerWakeUpEventHandler);
+        LOGGER.info("Should've registered the wake up event in the mod constructor.");
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("Common setup firing");
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-    }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-       }
+    private void playerWakeUpEventHandler(PlayerWakeUpEvent event) {
+        LOGGER.warn("PlayerWakeUpEvent fired and handler got it!");
     }
 }
